2f8dd1414eb2761f12cdce69f769730eceae2936
Santiago Priego <7561131662z@gmail.com>
2023-09-07 17:18:16 -0600

    Actualizacion_1

--- resources/views/alumnos/edit.blade.php
+++ resources/views/alumnos/edit.blade.php
@@ -5,87 +5,104 @@
 @section('titulo', 'Cursos create') {{-- El segundo parametro que se le pasa, es el titulo que va tener por defecto --}}
 
 @section('content')
-<h1>En esta pagina podras modificar un alumno</h1>
+<h1 class="font-bold text-4xl text-blue-600 mb-8 text-center">En esta pagina podras modificar un alumno</h1>
 
 <div class="md:flex md:justify-center md:gap-10 md:items-center">
+
+    <div class="md:w-6/12 p-5">
+        <img src="{{asset('img/edit.jpg')}}" alt="Imagen registro de usuarios">
+    </div>
+
 
     <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
 
         <form action="{{route('alumnos.update', $alumno)}}" method="post">
             @csrf
             @method('put')
-            <label for="">
-                Nombre
-                <br>
+
+            <div class="mb-2">
+                <label for="" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Nombre
+                    <br>
+            
+                    <input type="text" name="name" value="{{old('name', $alumno->name)}}" class="border p-3 w-full rounded-lg
+                    @error('name')
+                        border-red-500
+                    @enderror">
+            
+                </label>
+                    @error('name')
+                        <p>{{$message}}</p>
+                    @enderror
+            </div>
+
+            <div class="mb-2">
+                <label for="" class="mb-2 block uppercase text-gray-500 font-bold">Edad
+            
         
-                <input type="text" name="name" value="{{old('name', $alumno->name)}}" class="border p-3 w-full rounded-lg
-                @error('name')
-                    border-red-500
-                @enderror">
+                    <input type="number" name="edad" value="{{old('edad', $alumno->edad)}}" class="border p-3 w-full rounded-lg
+                    @error('edad')
+                        border-red-500
+                    @enderror">
+                    
+            
+                </label>
+                    @error('edad')
+                        <p>{{$message}}</p>
+                    @enderror
+            </div>
+
+
+            <div class="mb-2">
+                <label for="" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Materia:
+                    <br>
+            
+                    <input type="text" name="materia" value="{{old('materia', $alumno->materia)}}" class="border p-3 w-full rounded-lg
+                    @error('materia')
+                        border-red-500
+                    @enderror">
+            
+                </label>
+                    @error('materia')
+                        <p>{{$message}}</p>
+                    @enderror
+            </div>
+
+
+            <div class="mb-2">
+                <label for="" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Promedio:
+                    <br>
+            
+                    <input type="number" name="promedio" value="{{old('promedio', $alumno->promedio)}}" class="border p-3 w-full rounded-lg
+                    @error('promedio')
+                        border-red-500
+                    @enderror">
+            
+                </label>
+                    @error('promedio')
+                        <p>{{$message}}</p>
+                    @enderror
+            </div>
+
+
+            <div class="mb-2">
+                <div class="md:flex md:justify-between">
+                    <input type="submit" value="Modificar" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg">
         
-            </label>
-                @error('name')
-                    <p>{{$message}}</p>
-                @enderror
-            <br>
+            
+                <a href="{{route('alumnos.show', $alumno->id)}}" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg">Regresar</a>
+                </div>
+            </div>
         
-            <label for="">Edad
-            <br>
-        
-                <input type="number" name="edad" value="{{old('edad', $alumno->edad)}}" class="border p-3 w-full rounded-lg
-                @error('edad')
-                    border-red-500
-                @enderror">
-                
-        
-            </label>
-                @error('edad')
-                    <p>{{$message}}</p>
-                @enderror
-        
-            <br>
-        
-            <label for="">
-                Materia:
-                <br>
-        
-                <input type="text" name="materia" value="{{old('materia', $alumno->materia)}}" class="border p-3 w-full rounded-lg
-                @error('materia')
-                    border-red-500
-                @enderror">
-        
-            </label>
-                @error('materia')
-                    <p>{{$message}}</p>
-                @enderror
-            <br>
-        
-            <label for="">
-                Promedio:
-                <br>
-        
-                <input type="number" name="promedio" value="{{old('promedio', $alumno->promedio)}}" class="border p-3 w-full rounded-lg
-                @error('promedio')
-                    border-red-500
-                @enderror">
-        
-            </label>
-                @error('promedio')
-                    <p>{{$message}}</p>
-                @enderror
-            <br>
-        
-        
-        
-        
-            <input type="submit" value="Modificar" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg mt-4">
-        
+            
+            
+
         
         </form>
 
-        <div class="mt-5">
-            <a href="{{route('alumnos.show', $alumno->id)}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg">Regresar</a>
-        </div>
+        
 
 
     </div>
--- resources/views/alumnos/show.blade.php
+++ resources/views/alumnos/show.blade.php
@@ -4,29 +4,50 @@
 
 @section('content')
 
-<a href="{{route('alumnos.index')}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-2 text-white rounded-lg">Regresar</a>
-<a href="{{route('alumnos.edit', $alumno)}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-2 text-white rounded-lg">Editar</a>
 
 
+<div class="md:flex md:justify-center md:gap-10 md:items-center">
 
-<div class="mt-4 text-xl">
-    <p ><strong>Nombre: </strong> {{$alumno->name}}</p>
-    <p ><strong>Edad: </strong> {{$alumno->edad}}</p>
-    <p ><strong>Materia: </strong> {{$alumno->materia}}</p>
-    <p ><strong>Promedio: </strong> {{$alumno->promedio}}</p>
+    <div class="md:w-6/12 p-5">
+       
+        <img src="" alt="Imagen DEL ALUMNO">
+    </div>
+
+    <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+
+        <div class="mt-4 text-xl">
+
+            <div class="mb-10">
+                <a href="{{route('alumnos.index')}}" class="bg-green-600 hover:bg-green-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg">Regresar</a>
+                <a href="{{route('alumnos.edit', $alumno)}}" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-2 text-white rounded-lg">Editar</a>
+            </div>
+
+            <div class="mb-10">
+                <p class="mb-3"><strong>Nombre: </strong> {{$alumno->name}}</p>
+                <p class="mb-3"><strong>Edad: </strong> {{$alumno->edad}}</p>
+                <p class="mb-3"><strong>Materia: </strong> {{$alumno->materia}}</p>
+                <p class="mb-3"><strong>Promedio: </strong> {{$alumno->promedio}}</p>
+            </div>
+
+            
+  
+            <form action="{{route('alumnos.destroy', $alumno)}}" method="post" class="bg-red-600 hover:bg-red-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg text-center">
+                @csrf
+                @method('delete')
+            
+                
+                <button type="submit" class=" uppercase">Eliminar</button>
+            </form>
+
+
+            
+        </div>
+      
+
+    </div>
+
+
 </div>
-
-
-
-
-
-<form action="{{route('alumnos.destroy', $alumno)}}" method="post" class="bg-red-600 hover:bg-red-700 transition-colors cursor-pointer uppercase font-bold  p-2 text-white rounded-lg">
-    @csrf
-    @method('delete')
-
-    
-    <button type="submit">Eliminar</button>
-</form>
 
 
 @endsection
